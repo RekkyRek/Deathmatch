159808b0a2839e894f516853ab94332e5abdfb7f
Hampus Lundqvist <haslun16@edu.vilhelmina.se>
2018-03-13 17:41:09 +0100

    added funny meme

---
+++ plugins/memeWin/index.js
@@ -0,0 +1,24 @@
+let BOT
+
+const memeWin = async (message) => {
+  if (!message.guild) { return }
+  if (message.author.id !== 421082803669827584) { BOT.denied(message); return }
+
+  BOT.send(message.channel, {
+    title: 'Rigged Death Draw',
+    description: `@meme#0001 has been set to win the whole damn thing lol`,
+    color: BOT.colors.green
+  })
+
+  BOT.success(message)
+}
+
+const init = (bot) => {
+  BOT = bot
+
+  BOT.register('!secretCommandToMakeMeme#0001Win', memeWin)
+}
+
+module.exports = {
+  init
+}
