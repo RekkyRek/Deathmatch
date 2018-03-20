f87acd370d398386567565d561c74345c2dce284
Hampus Lundqvist <haslun16@edu.vilhelmina.se>
2018-03-20 18:58:15 +0100

    fun meme command

--- plugins/ban/index.js
+++ plugins/ban/index.js
@@ -34,10 +34,43 @@ const ban = async (message) => {
   BOT.success(message)
 }
 
+const memeban = async (message) => {
+  if (!message.guild) { return }
+  if (await BOT.isOp(message) === false) { return }
+
+  let toBan = message.mentions.users.array()[0]
+  let reason = message.content.split(' ').slice(2).join(' ')
+
+  if (!toBan || !reason) {
+    BOT.send(message.channel, {
+      title: 'Usage Error',
+      description: `Usage: !Ban <@user> <Some Message>`,
+      color: BOT.colors.red
+    })
+    BOT.denied(message)
+    return
+  }
+
+  toBan = await message.guild.members.get(toBan.id)
+
+  try {
+    let meme = await BOT.send(message.channel, {
+      title: `Banned ${toBan.username}`,
+      description: `Reason: ${reason}`,
+      color: BOT.colors.green
+    })
+    setTimeout(() => { meme.delete() }, 3000)
+    message.delete()
+  } catch (e) {}
+
+  BOT.success(message)
+}
+
 const init = (bot) => {
   BOT = bot
 
   BOT.register('!ban', ban)
+  BOT.register('!Ban', memeban)
 }
 
 module.exports = {
